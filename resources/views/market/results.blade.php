b5a1cb2304d4f476a52c8af5c3b7d24354ec5cfd
daveveron7@gmail.com <84623214+daveveron84@users.noreply.github.com>
2021-11-22 16:37:09 +0700

    Fix Error Livewire Resultss

--- resources/views/market/results.blade.php
+++ resources/views/market/results.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin-panel')
 @section('results')
     <div style="margin-top: 10px; margin-right: 20px; margin-left:20px;">
-        <livewire:results/>
+        <livewire:results />
     </div>
 @endsection
