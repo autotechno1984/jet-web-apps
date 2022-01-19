10c19274c86f183d633ff3cc2a77aa30157ffafa
daveveron7@gmail.com <84623214+daveveron84@users.noreply.github.com>
2022-01-19 22:33:17 +0700

    livewire config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Shanghai-Cobra</title>
+    <title>Grand Shanghai</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -31,7 +31,7 @@
             <div class="container">
                 <a href="#"></a>
                 <a class="navbar-brand" href="#" style="font-family: 'Salsa'">
-                    Shanghai - Cobra
+                    GRAND SHANGHAI
                 </a>
                 <button class="navbar-toggler" style="border: 1px solid white;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
